986c7d2fb666e191a032d4ff94044b9aebfb75f0
Alexandra <alexankitty@gmail.com>
2025-07-17 22:56:02 -0600

    add a command for changing the url

--- VoiceReplace.cs
+++ VoiceReplace.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 using System.Net.Http.Json;
+using Discord;
 
 namespace VoiceReplaceMedusa;
 
@@ -12,16 +13,21 @@ public sealed class VoiceReplace : Snek
     private readonly string _basePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
     private HttpClient _httpClient;
     private MedusaConfig _config;
+    private IDeserializer _deserializer;
+    private ISerializer _serializer;
     private readonly string _configYml = "config.yml";
     public async override ValueTask InitializeAsync()
     {
         try
         {
             _httpClient = new HttpClient();
-            var deserializer = new DeserializerBuilder()
+            _deserializer = new DeserializerBuilder()
                 .WithNamingConvention(CamelCaseNamingConvention.Instance)
                 .Build();
-            _config = deserializer.Deserialize<MedusaConfig>(File.ReadAllText(Path.Combine(_basePath, _configYml)));
+            _serializer = new SerializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+            _config = _deserializer.Deserialize<MedusaConfig>(File.ReadAllText(Path.Combine(_basePath, _configYml)));
         }
         catch (Exception ex)
         {
@@ -70,6 +76,20 @@ public sealed class VoiceReplace : Snek
             await ctx.SendErrorAsync("Failed to replace vocals.");
         }
         typing.Dispose();
+    }
+
+    [cmd]
+    [bot_owner_only]
+    public async Task VoiceReplacerApi(AnyContext ctx, string url = "")
+    {
+        if (string.IsNullOrWhiteSpace(url))
+        {
+            await ctx.SendConfirmAsync($"api: {_config.Api}");
+            return;
+        }
+        _config.Api = url.EndsWith('/') ? url.Substring(0, url.Length - 1) : url;
+        File.WriteAllText(Path.Combine(_basePath, _configYml), _serializer.Serialize(_config));
+        await ctx.ConfirmAsync();
     }
 }
 
